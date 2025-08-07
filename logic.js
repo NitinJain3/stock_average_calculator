d5bc3c2faa0a3c12bd26deca2fdc214a3326b12f
Nitin Jain <117276757+NitinJain3@users.noreply.github.com>
2025-08-07 23:48:16 +0530

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,48 +1,55 @@
-function _0x58da() {
-    var _0xda905c = ['5240IWfnMG', 'qty2', '111920cTqXvp', 'classList', 'result', '8KnwnGP', '6327rnlmZs', '12sFboOf', '577458kMDUIl', 'value', 'body', 'Please\x20enter\x20valid\x20quantities.', '992893LLgcTX', 'innerHTML', 'checked', '2525496nUEgYM', '5187917KyWXmP', 'qty1', '<br>\x0a\x20\x20\x20\x20\x20\x20\x20\x20<strong>Total\x20Units:</strong>\x20', 'toggle', 'price2', 'price1', '<br>\x0a\x20\x20\x20\x20\x20\x20\x20\x20<strong>Total\x20Investment:</strong>\x20₹', '88hhQumk', '1369rkvRST', '\x0a\x0a\x20\x20\x20\x20\x20\x20', 'toFixed', 'getElementById', '750pBoQil'];
-    _0x58da = function() {
-        return _0xda905c;
-    };
-    return _0x58da();
-}(function(_0x34cce4, _0x18e11b) {
-    var _0x172af5 = _0x32b5,
-        _0xd20fc5 = _0x34cce4();
+const _0x5c76fd = _0x3951;
+(function(_0x1db262, _0x26b2e8) {
+    const _0x1e4972 = _0x3951,
+        _0x5c546d = _0x1db262();
     while (!![]) {
         try {
-            var _0x24f470 = -parseInt(_0x172af5(0x19d)) / 0x1 * (-parseInt(_0x172af5(0x1a1)) / 0x2) + -parseInt(_0x172af5(0x1aa)) / 0x3 + -parseInt(_0x172af5(0x19c)) / 0x4 * (parseInt(_0x172af5(0x1a4)) / 0x5) + -parseInt(_0x172af5(0x194)) / 0x6 + -parseInt(_0x172af5(0x195)) / 0x7 * (-parseInt(_0x172af5(0x1a7)) / 0x8) + parseInt(_0x172af5(0x1a8)) / 0x9 * (parseInt(_0x172af5(0x1a2)) / 0xa) + -parseInt(_0x172af5(0x1ae)) / 0xb * (parseInt(_0x172af5(0x1a9)) / 0xc);
-            if (_0x24f470 === _0x18e11b) break;
-            else _0xd20fc5['push'](_0xd20fc5['shift']());
-        } catch (_0x16aacd) {
-            _0xd20fc5['push'](_0xd20fc5['shift']());
+            const _0x33deb3 = -parseInt(_0x1e4972(0x14f)) / 0x1 + parseInt(_0x1e4972(0x160)) / 0x2 + -parseInt(_0x1e4972(0x151)) / 0x3 + -parseInt(_0x1e4972(0x147)) / 0x4 * (parseInt(_0x1e4972(0x157)) / 0x5) + parseInt(_0x1e4972(0x152)) / 0x6 + parseInt(_0x1e4972(0x146)) / 0x7 * (parseInt(_0x1e4972(0x156)) / 0x8) + -parseInt(_0x1e4972(0x15f)) / 0x9 * (parseInt(_0x1e4972(0x14d)) / 0xa);
+            if (_0x33deb3 === _0x26b2e8) break;
+            else _0x5c546d['push'](_0x5c546d['shift']());
+        } catch (_0x11ef5a) {
+            _0x5c546d['push'](_0x5c546d['shift']());
         }
     }
-}(_0x58da, 0x6830d));
+}(_0x37f4, 0x30c35));
+
+function _0x37f4() {
+    const _0x4d30ea = ['393634godxSs', '7VzBlKG', '4TRjCiO', 'qty1', 'classList', 'change', 'addEventListener', '<br>\x0a\x20\x20\x20\x20\x20\x20<strong>Total\x20Investment:</strong>\x20₹', '70jdCtdx', 'dark-mode', '81502wRRwMY', 'toggle', '22131RfxdIE', '1746744cthuTU', 'qty2', '\x0a\x20\x20\x20\x20\x20\x20<strong>Average\x20Price:</strong>\x20₹', 'themeSwitcher', '2131928wLPBCj', '957905kChQRL', 'checked', 'getElementById', 'innerHTML', 'innerText', 'result', 'toFixed', 'value', '352593FQmWLb'];
+    _0x37f4 = function() {
+        return _0x4d30ea;
+    };
+    return _0x37f4();
+}
 
 function calculateAverage() {
-    var _0xe885 = _0x32b5,
-        _0x1f8279 = parseFloat(document['getElementById'](_0xe885(0x19a))['value']) || 0x0,
-        _0x2ae83d = parseFloat(document[_0xe885(0x1a0)](_0xe885(0x196))['value']) || 0x0,
-        _0x26cc2e = parseFloat(document[_0xe885(0x1a0)](_0xe885(0x199))[_0xe885(0x1ab)]) || 0x0,
-        _0x3e8fa5 = parseFloat(document['getElementById'](_0xe885(0x1a3))[_0xe885(0x1ab)]) || 0x0,
-        _0x3edad8 = _0x1f8279 * _0x2ae83d + _0x26cc2e * _0x3e8fa5,
-        _0xf79098 = _0x2ae83d + _0x3e8fa5;
-    if (_0xf79098 === 0x0) {
-        document['getElementById'](_0xe885(0x1a6))['innerText'] = _0xe885(0x1ad);
+    const _0x104035 = _0x3951,
+        _0x2a3a47 = parseFloat(document[_0x104035(0x159)]('price1')[_0x104035(0x15e)]),
+        _0xd4f172 = parseFloat(document['getElementById'](_0x104035(0x148))[_0x104035(0x15e)]),
+        _0x1b7130 = parseFloat(document[_0x104035(0x159)]('price2')[_0x104035(0x15e)]),
+        _0x2d2a24 = parseFloat(document[_0x104035(0x159)](_0x104035(0x153))['value']);
+    if (isNaN(_0x2a3a47) || isNaN(_0xd4f172) || isNaN(_0x1b7130) || isNaN(_0x2d2a24)) {
+        document['getElementById']('result')['innerText'] = 'Please\x20fill\x20all\x20fields\x20correctly.';
         return;
     }
-    var _0x3674e9 = _0x3edad8 / _0xf79098;
-    document['getElementById'](_0xe885(0x1a6))[_0xe885(0x1af)] = '\x0a\x20\x20\x20\x20\x20\x20\x20\x20<strong>Average\x20Price:</strong>\x20₹' + _0x3674e9[_0xe885(0x19f)](0x2) + _0xe885(0x197) + _0xf79098 + _0xe885(0x19b) + _0x3edad8[_0xe885(0x19f)](0x2) + _0xe885(0x19e);
+    if (_0x2a3a47 <= 0x0 || _0xd4f172 <= 0x0 || _0x1b7130 <= 0x0 || _0x2d2a24 <= 0x0) {
+        document[_0x104035(0x159)](_0x104035(0x15c))[_0x104035(0x15b)] = 'All\x20values\x20must\x20be\x20greater\x20than\x20zero.';
+        return;
+    }
+    const _0x4c77a6 = _0x2a3a47 * _0xd4f172 + _0x1b7130 * _0x2d2a24,
+        _0x249ece = _0xd4f172 + _0x2d2a24,
+        _0x223569 = _0x4c77a6 / _0x249ece;
+    document[_0x104035(0x159)](_0x104035(0x15c))[_0x104035(0x15a)] = _0x104035(0x154) + _0x223569[_0x104035(0x15d)](0x2) + '<br>\x0a\x20\x20\x20\x20\x20\x20<strong>Total\x20Units:</strong>\x20' + _0x249ece + _0x104035(0x14c) + _0x4c77a6[_0x104035(0x15d)](0x2) + '\x0a\x20\x20\x20\x20';
 }
 
-function _0x32b5(_0x12478d, _0x3ec39e) {
-    var _0x58daf8 = _0x58da();
-    return _0x32b5 = function(_0x32b5d8, _0x22f452) {
-        _0x32b5d8 = _0x32b5d8 - 0x193;
-        var _0x17f4c3 = _0x58daf8[_0x32b5d8];
-        return _0x17f4c3;
-    }, _0x32b5(_0x12478d, _0x3ec39e);
+function _0x3951(_0x1d8489, _0x15f888) {
+    const _0x37f45e = _0x37f4();
+    return _0x3951 = function(_0x39514a, _0x285fca) {
+        _0x39514a = _0x39514a - 0x146;
+        let _0x5f4c9f = _0x37f45e[_0x39514a];
+        return _0x5f4c9f;
+    }, _0x3951(_0x1d8489, _0x15f888);
 }
-document['getElementById']('themeSwitcher')['addEventListener']('change', function() {
-    var _0x29fb17 = _0x32b5;
-    document[_0x29fb17(0x1ac)][_0x29fb17(0x1a5)][_0x29fb17(0x198)]('dark-mode', this[_0x29fb17(0x193)]);
+document[_0x5c76fd(0x159)](_0x5c76fd(0x155))[_0x5c76fd(0x14b)](_0x5c76fd(0x14a), function() {
+    const _0x498e0a = _0x5c76fd;
+    document['body'][_0x498e0a(0x149)][_0x498e0a(0x150)](_0x498e0a(0x14e), this[_0x498e0a(0x158)]);
 });
